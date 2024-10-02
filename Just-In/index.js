3fef6b8ead4b7b4662a37fa2c6010eac6166df7d
Huijben <serge.huijben@cz.nl>
2024-10-02 16:52:24 +0200

    get calendar info

--- Just-In/index.js
+++ Just-In/index.js
@@ -6,6 +6,48 @@ function randomValueFromArray(array) {
   return array[randomNo];
 }
 
+async function getEvents() {
+  const logicAppUrl = 'https://logicapp-ip-diederik.azurewebsites.net:443/api/office_connection/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=N6TLr4SIRyzvll5Z_jEoRvgW6jlLTBVlJGfIbuUdxHs';
+  const startDate = new Date();
+  const endDate = new Date();
+
+  startDate.setHours(0, 0, 0, 0);
+  endDate.setHours(23, 59, 59, 999);
+
+  const response = await fetch(logicAppUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      startDate: startDate.toISOString(),
+      endDate: endDate.toISOString(),
+      organizer: 'fpa.ict.sales@cz.nl',
+    }),
+  });
+
+  let myEvents = `<p class="event">Geen events gevonden</p>`;
+  const data = await response.json();
+  if (data?.[0]?.["Locatie"]) {
+    myEvents = '';
+    data.map((event) => {
+      let location = event?.["Locatie"];
+      let dIndex = location?.indexOf('TD0');
+      let floor = '';
+      if (dIndex > -1) {
+        const dStart = dIndex + 3;
+        floor = location.substring(dStart, dStart + 1);
+        myEvents += `<p class="event">gebouw D, verdieping ${floor}</p>`;
+      } else if (location?.indexOf('TB0') > -1) {
+        myEvents += `<p class="event">gebouw B om ${event.Starttijd}</p>`;
+      } else {
+        myEvents += '';
+      }
+    });
+  }
+  document.querySelector('.events').innerHTML = myEvents || `<p class="event">Geen Locatie gevonden, wat dacht je van thuis werken?</p>`;
+}
+
 setInterval(() => {
   const randomChoice = randomValueFromArray(images);
   imgElem.src = `images/${randomChoice}.jpg`;
@@ -48,3 +90,5 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
+getEvents();
