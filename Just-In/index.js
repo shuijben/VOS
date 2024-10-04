e34a6cda74f974b3c07e825530dcc6d0582137c4
Huijben <serge.huijben@cz.nl>
2024-10-04 17:11:42 +0200

    update text and images

--- Just-In/index.js
+++ Just-In/index.js
@@ -4,6 +4,14 @@ const imgElem = document.querySelector('img');
 const output = document.querySelector('.events');
 const datumWeergave = document.querySelector('.datum');
 const weekdagWeergave = document.querySelector('.weekdag');
+let confettiOn = false;
+const isBirthday = setupConfetti();
+
+const birthdays = {
+  '07-10':'Danique',
+  '06-10': 'Rianca',
+  '09-10': 'Diederik', 
+}
 
 let abortController = null;
 
@@ -18,7 +26,7 @@ async function getEvents(startDate, endDate) {
   const dayOfWeek = startDate.getDay();
   setDatumWeergave(startDate, days[dayOfWeek]);
   if (dayOfWeek === 0 || dayOfWeek === 6) {
-    output.innerHTML = '<p class="event">Weekend, geen werk!</p>';
+    output.innerHTML = '<p class="event">Weekend, geen werk!</p>' + checkBirthday(startDate);
     setImageSource('off');
     return;
   }
@@ -54,9 +62,9 @@ async function getEvents(startDate, endDate) {
       myEvents = `<p class="event">${processEvent(data[0])}</p>`;
     }
     if (myEvents) {
-      output.innerHTML = myEvents;
+      output.innerHTML = myEvents + checkBirthday(startDate);
     } else {
-      output.innerHTML = `<p class="event">Jammer! Je roedel zit niet in D, kwispel naar je burcht in B</p>`;
+      output.innerHTML = `<p class="event">Jammer! Je roedel zit niet in D, kwispel naar je burcht in B</p>` + checkBirthday(startDate);
       setImageSource('burcht');
     } 
   } catch (error) {
@@ -89,9 +97,20 @@ function processEvent(event) {
   }
 }
 
+function checkBirthday(date) {
+  const birthday = birthdays[`${addZero(date.getDate())}-${addZero(date.getMonth() + 1)}`];
+  if (birthday) {
+    confettiOn = true;
+    isBirthday?.draw();
+    return `<p class="event">Hoera, vandaag is ${birthday} jarig!</p>`;
+  }
+  confettiOn = false;
+  return '';
+}
+
 function setDatumWeergave(date, weekdag) {
   weekdagWeergave.innerHTML = `${weekdag}`;
-  datumWeergave.innerHTML = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+  datumWeergave.innerHTML = `${addZero(date.getDate())}-${addZero(date.getMonth() + 1)}-${date.getFullYear()}`;
 }
 
 function setImageSource(floor) {
@@ -159,3 +178,111 @@ vorige.addEventListener('click', () => {
   
   getEvents(peilData.startDate, peilData.endDate);
 });
+
+// Confetti
+function setupConfetti() {
+
+  let W = window.innerWidth;
+  let H = window.innerHeight;
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const maxConfettis = 150;
+  const particles = [];
+
+  const possibleColors = [
+    "DodgerBlue",
+    "OliveDrab",
+    "Gold",
+    "Pink",
+    "SlateBlue",
+    "LightBlue",
+    "Gold",
+    "Violet",
+    "PaleGreen",
+    "SteelBlue",
+    "SandyBrown",
+    "Chocolate",
+    "Crimson"
+  ];
+
+  function randomFromTo(from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+  }
+
+  function confettiParticle() {
+    this.x = Math.random() * W; // x
+    this.y = Math.random() * H - H; // y
+    this.r = randomFromTo(11, 33); // radius
+    this.d = Math.random() * maxConfettis + 11;
+    this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+    this.tilt = Math.floor(Math.random() * 33) - 11;
+    this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+    this.tiltAngle = 0;
+    
+    this.draw = function() {
+      context.beginPath();
+      context.lineWidth = this.r / 2;
+      context.strokeStyle = this.color;
+      context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+      context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+      return context.stroke();
+    };
+  }
+
+  function Draw() {
+    const results = [];
+    
+    context.clearRect(0, 0, W, window.innerHeight);
+    if (confettiOn) {
+      // Magical recursive functional love
+      requestAnimationFrame(Draw);
+      
+      for (var i = 0; i < maxConfettis; i++) {
+        results.push(particles[i].draw());
+      }
+      
+      let particle = {};
+      let remainingFlakes = 0;
+      for (var i = 0; i < maxConfettis; i++) {
+        particle = particles[i];
+        
+        particle.tiltAngle += particle.tiltAngleIncremental;
+        particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+        particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+        
+        if (particle.y <= H) remainingFlakes++;
+        
+        // If a confetti has fluttered out of view,
+        // bring it back to above the viewport and let if re-fall.
+        if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+          particle.x = Math.random() * W;
+          particle.y = -30;
+          particle.tilt = Math.floor(Math.random() * 10) - 20;
+        }
+      }
+    }    
+    return results;
+  }
+
+  window.addEventListener(
+    "resize",
+    function() {
+      W = window.innerWidth;
+      H = window.innerHeight;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    },
+    false
+  );
+
+  // Push new confetti objects to `particles[]`
+  for (var i = 0; i < maxConfettis; i++) {
+    particles.push(new confettiParticle());
+  }
+
+  // Initialize
+  canvas.width = W;
+  canvas.height = H;
+  return { draw: Draw };
+}
