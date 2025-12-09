f2664ab29f1a8940604c907ff9f237dfdd7a1697
shuijben <162966547+shuijben@users.noreply.github.com>
2025-12-09 17:15:25 +0100

    update caching strategy

--- Just-In/sw.js
+++ Just-In/sw.js
@@ -1,28 +1,10 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('vos-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/index.js',
-      '/style.css',
-      '/favicon.ico',
-      '/images/vos0.jpg',
-      '/images/vos1.jpg',
-      '/images/vos2.jpg',
-      '/images/vos3.jpg',
-      '/images/vos4.jpg',
-      '/images/vos5.jpg',
-      '/images/vosburcht.jpg',
-      '/images/vosoff.jpg',
-      '/images/voszoekt.jpg',
-      '/fonts/TheMixB4-7_Bold.woff',
-      '/fonts/TheMixB4-7_Bold.woff2',
-      '/fonts/TheSansB4-5_Plain.woff',
-      '/fonts/TheSansB4-5_Plain.woff2',
-      '/fonts/TheSansB4-5iPlainItalic.woff',
-      '/fonts/TheSansB4-5iPlainItalic.woff2',
-      '/fonts/TheSansB4-7_Bold.woff',
-      '/fonts/TheSansB4-7_Bold.woff2',
+      '*',
+      'images/*.jpg',
+      'fonts/*.woff',
+      'fonts/*.woff2',
     ])),
   );
 });
