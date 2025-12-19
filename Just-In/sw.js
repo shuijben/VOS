6655d5dfc7d8c79ba44646e754fdf882542d9a39
shuijben <serge.huijben@cz.nl>
2025-12-19 16:36:38 +0100

    update serviceworker

--- Just-In/sw.js
+++ Just-In/sw.js
@@ -1,10 +1,8 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('vos-store').then((cache) => cache.addAll([
-      './index.html',
-      './index.js',
-      './style.css',
-      './favicon.ico',
+      './icon/fox-icon.png',
+      './icon/fox-icon-512.png',
       './images/vos0.jpg',
       './images/vos1.jpg',
       './images/vos2.jpg',
@@ -29,32 +27,26 @@ self.addEventListener('install', (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  const url = e?.request?.url || '';
-
-  if (url) {
-    console.log(`fetching - url: ${url}`);
-    e.respondWith(caches.open('vos-store').then((cache) => {
-      //respond to get favicon.ico requests
-      if (url.endsWith('favicon.ico')) {
-        console.log('favicon requested, respond with the one cached');
-        return cache.match('./favicon.ico').then((cachedResponse) => {
-          console.log('returning cached favicon');
-          return cachedResponse;
-        });
-      }
+self.addEventListener('fetch', (event) => {
+  const destination = event?.request?.destination || '';
+  
+  if (['image', 'font'].includes(destination)) {
+    const url = event.request.url;
+    console.log(`fetching - url: ${url}, destination: ${event.request.destination}`);
+    event.respondWith(caches.open('vos-store').then((cache) => {
       // Go to the cache first
-      return cache.match(e.request.url).then((cachedResponse) => {
+      return cache.match(event.request.url).then((cachedResponse) => {
+        console.log(cachedResponse ? `found in cache: ${url}` : `not found in cache: ${url}`);
         // Return a cached response if we have one
         if (cachedResponse) {
-          console.log(cachedResponse ? `found in cache: ${url}` : `not found in cache: ${url}`);
           return cachedResponse;
         }
 
         // Otherwise, hit the network
-        return fetch(e.request).then((fetchedResponse) => {
+        return fetch(event.request).then((fetchedResponse) => {
           // Add the network response to the cache for later visits
-          cache.put(e.request, fetchedResponse.clone());
+          console.log(`caching new resource: ${url}`);
+          cache.put(event.request, fetchedResponse.clone());
 
           // Return the network response
           return fetchedResponse;
@@ -62,6 +54,7 @@ self.addEventListener('fetch', (e) => {
       });
     }));
   } else {
+    console.log(`not handling fetch for destination: ${destination}`);
     return;
   }
 });
