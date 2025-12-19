9d5a8708cf28904d9542e76f2e1c480d10dcf9aa
shuijben <serge.huijben@cz.nl>
2025-12-19 14:44:00 +0100

    update serviceworker

--- Just-In/sw.js
+++ Just-In/sw.js
@@ -12,6 +12,9 @@ self.addEventListener('install', (e) => {
       './images/vos4.jpg',
       './images/vos5.jpg',
       './images/vosburcht.jpg',
+      './images/vosKerst.jpg',
+      './images/vosNieuwjaar.jpg',
+      './images/vosthuis.jpg',
       './images/vosoff.jpg',
       './images/voszoekt.jpg',
       './fonts/TheMixB4-7_Bold.woff',
@@ -27,8 +30,40 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log(`fetching - url: ${e.request.url}`);
+  const url = e?.request?.url || '';
+
+  if (url) {
+    console.log(`fetching - url: ${url}`);
+    e.respondWith(caches.open('vos-store').then((cache) => {
+      //respond to get favicon.ico requests
+      if (url.endsWith('favicon.ico')) {
+        console.log('favicon requested, respond with the one cached');
+        return cache.match('./favicon.ico').then((cachedResponse) => cachedResponse);
+      }
+      // Go to the cache first
+      return cache.match(e.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          console.log(cachedResponse ? `found in cache: ${url}` : `not found in cache: ${url}`);
+          return cachedResponse;
+        }
+
+        // Otherwise, hit the network
+        return fetch(e.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(e.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
+    }));
+  } else {
+    return;
+  }
+
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    }));
 });
